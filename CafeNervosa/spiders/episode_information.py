6e6dc985c91df4ed9324eb621a534392b712f12b
iteague <ikurtteague@gmail.com>
2016-09-27 15:47:30 -0400

    Simple Frasier Scraping Tool

---
+++ CafeNervosa/spiders/episode_information.py
@@ -0,0 +1,35 @@
+import scrapy 
+
+class EpisodeInformation(scrapy.Spider):
+	name = 'episodes'
+	allowed_domains =['frasieronline.co.uk']
+	start_urls = [
+		'http://www.frasieronline.co.uk/episodeguide/'
+	]
+	
+	
+	def parse(self, response):
+		
+		seasons = response.xpath("//tr//p[1]/a/@href").extract()	
+		
+		for season in seasons:
+			episodePage = response.urljoin(season)
+			yield scrapy.Request(episodePage, callback=self.parse_season)
+			
+	def parse_season(self, response):
+		
+		def extract_with_xpath(query):
+			
+			return response.xpath(query).extract()
+		yield {
+			'Page Title': extract_with_xpath("head/title/text()"),
+		
+			
+		}
+		
+		
+
+
+			
+		
+
